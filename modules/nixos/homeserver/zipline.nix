62ea9e7b88d5da66b21631b1dfddf2b9f288b0b8
wo2wz <189177184+wo2wz@users.noreply.github.com>
2025-08-27 17:31:48 -0400

    fix zipline user/group and adjust port

--- modules/nixos/homeserver/zipline.nix
+++ modules/nixos/homeserver/zipline.nix
@@ -1,9 +1,16 @@
 { config, ... }:
 
 {
+  users.users.zipline = {
+    group = "zipline";
+    isSystemUser = true;
+  };
+  users.groups.zipline = {};
   services.zipline = {
     enable = true;
     settings = {
+      CORE_PORT = 8001;
+
       FEATURES_VERSION_CHECKING = "false";
       FEATURES_THUMBNAILS_NUM_THREADS = 2;
     };
