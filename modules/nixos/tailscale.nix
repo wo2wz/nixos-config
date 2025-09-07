53673402dd35c5078381de40e93646386cb98c10
wo2wz <189177184+wo2wz@users.noreply.github.com>
2025-09-06 20:01:56 -0400

    switch tailscale to unstable to avoid build failure

--- modules/nixos/tailscale.nix
+++ modules/nixos/tailscale.nix
@@ -1,5 +1,7 @@
-{ config, ... }:
+{ config, inputs, pkgs, ... }:
 
 {
   services.tailscale.enable = true;
+  # fix build failure temporarily
+  services.tailscale.package = inputs.nixpkgs-unstable.legacyPackages.${pkgs.system}.tailscale;
 }
