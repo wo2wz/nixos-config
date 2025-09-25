84995e15bb6512b23866fd7d22c22db2f8360e49
wo2wz <189177184+wo2wz@users.noreply.github.com>
2025-09-25 17:19:47 -0400

    remove wo2w group actually

--- common/users.nix
+++ common/users.nix
@@ -1,15 +1,12 @@
 { hostName, config, ... }:
 
 {
-  users = {
-    users.wo2w = {
-      isNormalUser = true;
-      description = "${hostName}";
-      extraGroups = [ "networkmanager" "wheel" ];
+  users.users.wo2w = {
+    isNormalUser = true;
+    description = "${hostName}";
+    extraGroups = [ "networkmanager" "wheel" ];
 
-      # make new user logins (iso/vm/new machine) use a default password
-      initialPassword = "1234";
-    };
-    groups.wo2w = {};
+    # make new user logins (iso/vm/new machine) use a default password
+    initialPassword = "1234";
   };
 }
