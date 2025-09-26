2fca799117b2227a2be7e34c2e27272361065277
wo2wz <189177184+wo2wz@users.noreply.github.com>
2025-09-25 22:56:44 -0400

    add music nextcloud app

--- modules/nixos/homeserver/nextcloud.nix
+++ modules/nixos/homeserver/nextcloud.nix
@@ -32,7 +32,7 @@
 
       maxUploadSize = "200G";
       extraApps = {
-        inherit (config.services.nextcloud.package.packages.apps) calendar deck onlyoffice tasks twofactor_webauthn user_oidc;
+        inherit (config.services.nextcloud.package.packages.apps) calendar deck onlyoffice tasks music twofactor_webauthn user_oidc;
       };
     };    
 
