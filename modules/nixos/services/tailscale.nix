dc83e172b92cf2943fa1f2ffc93fcecaf7db9f15
wo2wz <189177184+wo2wz@users.noreply.github.com>
2025-10-11 18:55:15 -0400

    tailscale: move back to stable

--- modules/nixos/services/tailscale.nix
+++ modules/nixos/services/tailscale.nix
@@ -1,7 +1,5 @@
-{ config, inputs, pkgs, ... }:
+{ config, ... }:
 
 {
   services.tailscale.enable = true;
-  # fix build failure temporarily
-  services.tailscale.package = inputs.nixpkgs-unstable.legacyPackages.${pkgs.system}.tailscale;
 }
