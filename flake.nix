22c1972a7e0ef7b220e5bf0fc0dbf408192e7f88
wo2wz <189177184+wo2wz@users.noreply.github.com>
2025-10-12 13:42:21 -0400

    plasma: kill plasma-manager (it has not been used)

--- flake.nix
+++ flake.nix
@@ -30,12 +30,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    plasma-manager = {
-      url = "github:nix-community/plasma-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.home-manager.follows = "home-manager";
-    };
-
     niri = {
       url = "github:sodiboo/niri-flake";
       inputs.nixpkgs.follows = "nixpkgs";
