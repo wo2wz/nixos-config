743160d7453b19f8faf260db914be5d813fac2c3
wo2wz <189177184+wo2wz@users.noreply.github.com>
2025-10-20 18:13:31 -0400

    librewolf: remove unused extensions and modify librewolf settings to mitigate annoyances

--- modules/home/librewolf.nix
+++ modules/home/librewolf.nix
@@ -3,9 +3,17 @@
 {
   programs.librewolf = {
     enable = true;
-    nativeMessagingHosts = [ pkgs.kdePackages.plasma-browser-integration ];
     settings = {
       "identity.fxaccounts.enabled" = true;
+
+      "privacy.resistFingerprinting" = false;
+      "privacy.fingerprintingProtection" = true;
+      "privacy.fingerprintingProtection.overrides" = "+AllTargets,-CSSPrefersColorScheme";
+      "webgl.disabled" = false;
+      "privacy.clearOnShutdown.cookies" = false;
+      "privacy.clearOnShutdown.downloads" = true;
+      "privacy.clearOnShutdown.history" = false;
+
       "clipboard.autocopy" = false;
     };
     policies = {
@@ -27,10 +35,8 @@
           (extension "decentraleyes" "jid1-BoFifL9Vbdl2zQ@jetpack")
           (extension "happy-bonobo-disable-webrtc" "jid1-5Fs7iTLscUaZBgwr@jetpack")
           (extension "enhancer-for-youtube" "enhancerforyoutube@maximerf.addons.mozilla.org")
-          (extension "private-relay" "private-relay@firefox.com")
           (extension "indie-wiki-buddy" "{cb31ec5d-c49a-4e5a-b240-16c767444f62}")
           (extension "libredirect" "7esoorv3@alefvanoon.anonaddy.me")
-          (extension "plasma-integration" "plasma-browser-integration@kde.org")
           (extension "privacy-badger17" "jid1-MnnxcxisBPnSXQ@jetpack")
           (extension "return-youtube-dislikes" "{762f9885-5a13-4abd-9c77-433dcd38b8fd}")
           (extension "ublock-origin" "uBlock0@raymondhill.net")
@@ -43,13 +49,15 @@
         privateDefault = "ddg";
       };
       settings = {
-        "browser.urlbar.suggest.history" = false;
+        "browser.urlbar.suggest.history" = true;
         "browser.urlbar.suggest.bookmark" = false;
         "browser.urlbar.suggest.openpage" = false;
         "browser.urlbar.suggest.topsites" = false;
         "browser.urlbar.suggest.recentsearches" = false;
+        # remember tabs on startup
         "browser.startup.page" = 3;
 
+        # encrypted dns
         "network.trr.mode" = 2;
         "network.trr.uri" = "https://base.dns.mullvad.net/dns-query";
         "network.trr.custom_uri" = "https://base.dns.mullvad.net/dns-query";
@@ -61,14 +69,9 @@
         "privacy.clearOnShutdown.sessions" = false;
         "privacy.donottrackheader.enabled" = true;
 
+        # use xdg file picker instead of builtin browser picker
         "widget.use-xdg-desktop-portal.file-picker" = 1;
       };
     };
-  };
- 
-  # symlink for plasma browser integration native messaging in librewolf
-  home.file.".librewolf/native-messaging-hosts" = {
-    enable = true;
-    source = "${pkgs.kdePackages.plasma-browser-integration}/lib/mozilla/native-messaging-hosts";
   };
 }
