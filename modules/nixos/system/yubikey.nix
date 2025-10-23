52a68c005a9ede72577f6caf82efde5f79403431
wo2wz <189177184+wo2wz@users.noreply.github.com>
2025-10-22 22:40:14 -0400

    yubikey: remove unnecessary conditional and add u2f settings

--- modules/nixos/system/yubikey.nix
+++ modules/nixos/system/yubikey.nix
@@ -2,8 +2,15 @@
 
 {
   # enable yubikey u2f for use with pam
-  security.pam.services = {
-    sudo.u2fAuth = true;
-    polkit-1.u2fAuth = if config.services.desktopManager.plasma6.enable then true else false;
+  security.pam = {
+    services = {
+      sudo.u2fAuth = true;
+      polkit-1.u2fAuth = true;
+    };
+
+    u2f.settings = {
+      authfile = "/etc/Yubico/u2f_keys";
+      cue = true;
+    };
   };
 }
