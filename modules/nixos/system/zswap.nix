5ebde3599f2a25bfb2f3fca2b402757bcf4f66ed
wo2wz <189177184+wo2wz@users.noreply.github.com>
2025-10-24 12:32:48 -0400

    zswap: use default compressor
    lz4 isnt working and i dont know why but who cares

--- modules/nixos/system/zswap.nix
+++ modules/nixos/system/zswap.nix
@@ -1,18 +1,13 @@
 { config, ... }:
 
 {
-  boot = {
-    # necessary for lz4 compressor
-    initrd.systemd.enable = true;
-
-    kernelParams =
-    # zswap needs a swapfile
-    assert config.swapDevices != [];
-    [
-      "zswap.enabled=1"
-      "zswap.compressor=lz4"
-      "zswap.max_pool_percent=25"
-      "zswap.shrinker_enabled=1"
-    ];
-  };
+  boot.kernelParams =
+  # zswap needs a swapfile
+  assert config.swapDevices != [];
+  [
+    "zswap.enabled=1"
+    "zswap.compressor=zstd"
+    "zswap.max_pool_percent=25"
+    "zswap.shrinker_enabled=1"
+  ];
 }
