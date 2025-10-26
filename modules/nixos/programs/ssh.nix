eee7d30a91966197612b46910044387762490264
wo2wz <189177184+wo2wz@users.noreply.github.com>
2025-10-26 19:23:36 -0400

    ssh: remove agent and askpass
    fuck so this is why gnome-keyring ssh isnt working :sob:

--- modules/nixos/programs/ssh.nix
+++ modules/nixos/programs/ssh.nix
@@ -1,19 +1,15 @@
 { config, pkgs, ... }:
 
 {
-  programs.ssh = {
-    startAgent = true;
-    enableAskPassword = true;
-    extraConfig = "
-      IdentityFile /home/wo2w/.ssh/yubikey
-      User wo2w
-      Port 8743
-      Host gameserver
-        Hostname 192.168.2.112
-      Host Swordsmachine
-        Hostname 192.168.2.74
-      Host Earthmover
-        Hostname 192.168.2.175
-    ";
-  };
+  programs.ssh.extraConfig = "
+    IdentityFile /home/wo2w/.ssh/yubikey
+    User wo2w
+    Port 8743
+    Host gameserver
+      Hostname 192.168.2.112
+    Host Swordsmachine
+      Hostname 192.168.2.74
+    Host Earthmover
+      Hostname 192.168.2.175
+  ";
 }
