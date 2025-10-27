556e4377353b5cf8e2f6faf1b96fb50c6c8c3486
wo2wz <189177184+wo2wz@users.noreply.github.com>
2025-10-26 20:35:45 -0400

    ssh: gameserver -> Gutterman

--- modules/nixos/programs/ssh.nix
+++ modules/nixos/programs/ssh.nix
@@ -5,7 +5,7 @@
     IdentityFile /home/wo2w/.ssh/yubikey
     User wo2w
     Port 8743
-    Host gameserver
+    Host Gutterman
       Hostname 192.168.2.112
     Host Swordsmachine
       Hostname 192.168.2.74
