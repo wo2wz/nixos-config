c73720458a35c8af1b49513379ade61a158f62ba
wo2wz <189177184+wo2wz@users.noreply.github.com>
2025-10-27 14:35:02 -0400

    Swordsmachine: disable nvidiasettings gui

--- hosts/Swordsmachine/default.nix
+++ hosts/Swordsmachine/default.nix
@@ -32,13 +32,17 @@
   specialisation.disable-dgpu.configuration.hardware.nvidiaOptimus.disable = true;
 
   # pin the latest nvidia driver that works because they are so awesome in releasing an update that broke opengl for my 1050ti
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-    version = "570.144";
-    sha256_64bit = "sha256-wLjX7PLiC4N2dnS6uP7k0TI9xVWAJ02Ok0Y16JVfO+Y=";
-    sha256_aarch64 = "sha256-6kk2NLeKvG88QH7/YIrDXW4sgl324ddlAyTybvb0BP0=";
-    openSha256 = "sha256-PATw6u6JjybD2OodqbKrvKdkkCFQPMNPjrVYnAZhK/E=";
-    settingsSha256 = "sha256-VcCa3P/v3tDRzDgaY+hLrQSwswvNhsm93anmOhUymvM=";
-    persistencedSha256 = "sha256-hx4w4NkJ0kN7dkKDiSOsdJxj9+NZwRsZEuhqJ5Rq3nM=";
+  hardware.nvidia = {
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "570.144";
+      sha256_64bit = "sha256-wLjX7PLiC4N2dnS6uP7k0TI9xVWAJ02Ok0Y16JVfO+Y=";
+      sha256_aarch64 = "sha256-6kk2NLeKvG88QH7/YIrDXW4sgl324ddlAyTybvb0BP0=";
+      openSha256 = "sha256-PATw6u6JjybD2OodqbKrvKdkkCFQPMNPjrVYnAZhK/E=";
+      settingsSha256 = "sha256-VcCa3P/v3tDRzDgaY+hLrQSwswvNhsm93anmOhUymvM=";
+      persistencedSha256 = "sha256-hx4w4NkJ0kN7dkKDiSOsdJxj9+NZwRsZEuhqJ5Rq3nM=";
+    };
+
+    nvidiaSettings = false;
   };
 
   environment.etc."Yubico/u2f_keys".text = "wo2w:RFdNibvUq8C38apD6K7kw116TfPBBbOf1PdQJrpAtorqV9JGvOTvVK71BR0CvlFCQ8/i+Dr/9D+H3QX5XIy+4g==,Q/N8eJG6lqrczqZQOZ1/Q956ufeORn4GlXuD//IC9swlbohg7lg1bF7wxJ1e8w66hb+vvoCrTrE0wTlbtsEbCw==,es256,+presence";
