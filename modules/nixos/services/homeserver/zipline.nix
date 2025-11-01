ca1d1d0a32c9500096659bdcc6f99559c434ef95
wo2wz <189177184+wo2wz@users.noreply.github.com>
2025-11-01 13:49:04 -0400

    zipline: add more settings
    decided to wipe zipline's db and restart to fix it behaving oddly, motivated me to do this
    edit: it did not work (goofy read-only fs error even when it has 777 permissions??)

--- modules/nixos/services/homeserver/zipline.nix
+++ modules/nixos/services/homeserver/zipline.nix
@@ -21,12 +21,24 @@
   services.zipline = {
     enable = true;
     settings = {
+      CORE_DEFAULT_DOMAIN = "zipline.wo2wz.fyi";
       CORE_PORT = 8001;
+      CORE_TRUST_PROXY = "true";
+      CORE_RETURN_HTTPS_URLS = "true";
+
       DATASOURCE_LOCAL_DIRECTORY = "/mnt/external/storage/zipline/uploads";
 
       FEATURES_VERSION_CHECKING = "false";
       FEATURES_THUMBNAILS_NUM_THREADS = 2;
-      FEATURES_OAUTH_REGISTRATION = "true";
+      FEATURES_ROBOTS_TXT = "false";
+      INVITES_ENABLED = "false";
+
+      MFA_TOTP_ENABLED = "true";
+      MFA_PASSKEYS = "true";
+
+      FILES_MAX_FILE_SIZE = "3091283091716487142128741263894122347014687124687124614791824619246129491246128461841279468127468912461924612974182746182468712468126487912648126481256487126491672941974612945618274610289417846192849712471eb";
+      FILES_ASSUME_MIMETYPES = "true";
+      FILES_REMOVE_GPS_METADATA = "true";
     };
     environmentFiles = [ config.sops.secrets."zipline/secrets.env".path ];
   };
