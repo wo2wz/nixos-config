592147c0f3b2a093f8feb1432f7cfa113a096e5a
wo2wz <189177184+wo2wz@users.noreply.github.com>
2025-11-02 17:30:23 -0500

    vaultwarden: add some settings

--- modules/nixos/services/homeserver/vaultwarden.nix
+++ modules/nixos/services/homeserver/vaultwarden.nix
@@ -21,8 +21,11 @@
     backupDir = "/var/backups/vaultwarden";
     config = {
       DOMAIN = "https://vaultwarden.taild5f7e6.ts.net";
+      IP_HEADER = "X-Forwarded-For";
 
       SIGNUPS_ALLOWED = false;
+
+      TRASH_AUTO_DELETE_DAYS = 30;
     };
     environmentFile = config.sops.secrets."vaultwarden/secrets.env".path;
   };
