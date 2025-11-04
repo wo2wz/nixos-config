a9be4f1144d05f90b2159e564f34910b09b43cd5
wo2wz <189177184+wo2wz@users.noreply.github.com>
2025-11-03 21:00:57 -0500

    Swordsmachine: remove nvidia driver pin
    decided to ball since it has been a while and opengl is no longer fucked, maybe it will be fine now

--- hosts/Swordsmachine/default.nix
+++ hosts/Swordsmachine/default.nix
@@ -33,16 +33,8 @@
   # also the longest one-line option name in this config
   specialisation.disable-dgpu.configuration.hardware.nvidiaOptimus.disable = true;
 
-  # pin the latest nvidia driver that works because they are so awesome in releasing an update that broke opengl for my 1050ti
   hardware.nvidia = {
-    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "570.144";
-      sha256_64bit = "sha256-wLjX7PLiC4N2dnS6uP7k0TI9xVWAJ02Ok0Y16JVfO+Y=";
-      sha256_aarch64 = "sha256-6kk2NLeKvG88QH7/YIrDXW4sgl324ddlAyTybvb0BP0=";
-      openSha256 = "sha256-PATw6u6JjybD2OodqbKrvKdkkCFQPMNPjrVYnAZhK/E=";
-      settingsSha256 = "sha256-VcCa3P/v3tDRzDgaY+hLrQSwswvNhsm93anmOhUymvM=";
-      persistencedSha256 = "sha256-hx4w4NkJ0kN7dkKDiSOsdJxj9+NZwRsZEuhqJ5Rq3nM=";
-    };
+    package = config.boot.kernelPackages.nvidiaPackages.production;
 
     nvidiaSettings = false;
   };
