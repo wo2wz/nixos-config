432d424167cf8831976f7028367d6084d2d467ee
wo2wz <189177184+wo2wz@users.noreply.github.com>
2025-11-30 19:49:01 -0500

    flake: update to nixos stable 25.11

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -3,9 +3,12 @@
 {
   programs.git = {
     enable = true;
-    userName = "wo2wz";
-    userEmail = "189177184+wo2wz@users.noreply.github.com";
-    extraConfig = {
+    settings = {
+      user = {
+        name = "wo2wz";
+        email = "189177184+wo2wz@users.noreply.github.com";
+      };
+
       init.defaultBranch = "main";
       safe.directory = "/etc/nixos";
     };
