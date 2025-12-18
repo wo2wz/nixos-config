b5410abd331aee33a4e1a368b8824a82d7506c3f
wo2wz <189177184+wo2wz@users.noreply.github.com>
2025-12-18 12:49:24 -0500

    syncthing: add declarative secrets for Drone

--- modules/nixos/services/syncthing/server.nix
+++ modules/nixos/services/syncthing/server.nix
@@ -1,25 +1,35 @@
 { config, ... }:
 
 {
-  services.syncthing.settings.folders = {
-    minecraft-instances = {
-      path = "${config.services.syncthing.dataDir}/minecraft-instances";
-      type = "receiveonly";
-      devices = [
-        "drone"
-        "earthmover"
-        "swordsmachine"
-      ];
-    };
+  sops.secrets = {
+    "syncthing/cert.pem" = {};
+    "syncthing/key.pem" = {};
+  };
 
-    terraria = {
-      path = "${config.services.syncthing.dataDir}/terraria";
-      type = "receiveonly";
-      devices = [
-        "drone"
-        "earthmover"
-        "swordsmachine"
-      ];
+  services.syncthing = {
+    cert = config.sops.secrets."syncthing/cert.pem".path;
+    key = config.sops.secrets."syncthing/key.pem".path;
+
+    settings.folders = {
+      minecraft-instances = {
+        path = "${config.services.syncthing.dataDir}/minecraft-instances";
+        type = "receiveonly";
+        devices = [
+          "drone"
+          "earthmover"
+          "swordsmachine"
+        ];
+      };
+
+      terraria = {
+        path = "${config.services.syncthing.dataDir}/terraria";
+        type = "receiveonly";
+        devices = [
+          "drone"
+          "earthmover"
+          "swordsmachine"
+        ];
+      };
     };
   };
 }
